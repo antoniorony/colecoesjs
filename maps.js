6a3437a70a51b7144f19767fb8afa1cd01aa90e4
antoniorony <antoniorony.dsn.pack@alterdata.com.br>
2022-03-28 22:55:01 -0300

    Initial commit

---
+++ maps.js
@@ -0,0 +1,16 @@
+class Maps {
+    
+    constructor() {
+        let usuarios = new Map();
+        usuarios.set('Luiz', 'Admin');
+        usuarios.set('Stephany', 'Admin');
+        usuarios.set('Jorge', 'User');
+
+    }
+
+    getAdmins(map) {
+
+    }
+
+    
+}
